55b8880a75e2b45340d2bd72e559744aca36c5dc
Chris Nady <nady.christopher@gmail.com>
2017-09-06 12:58:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,11 @@
 # Write your code here.
+def badge_maker (name)
+  "Hello, my name is #{name}"
+end
+
 def batch_badge_creator (names)
+  new_array = []
   names.each do |name|
-    puts "Hello, my name is #{name}"
+    new_array.push(badge_maker(name))
   end
 end
